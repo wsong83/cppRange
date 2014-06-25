ae3afef80a8cddd1a76b2656e38cc616f5f7e9a3
Wei Song <wsong83@gmail.com>
2014-06-26 00:16:17 +0100

    try to compile the string to range functions

--- include/cpp_range_util.hpp
+++ include/cpp_range_util.hpp
@@ -27,9 +27,16 @@
 #ifndef _CPP_RANGE_UTIL_H_
 #define _CPP_RANGE_UTIL_H_
 
+// just in case this header is used separately
+#include "cpp_range_util_def.hpp"
+
+#ifndef _CPP_RANGE_ELEMENT_H_
+#include "cpp_range_element.hpp"
+#endif
+
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
-#include <boost/foreach.hpp>
+#include <boost/xpressive/xpressive.hpp>
 #include <list>
 #include <string>
 
@@ -42,57 +49,56 @@ namespace CppRange {
 
   // parse a simple range
   template<class T>
-  std::pair<T,T> range_element_paser(const std::string& str_arg) {
+  RangeElement<T> paser_range(const std::string& str_arg) {
+    // parse something like "[3:0]", "[4]" or even "[]"
+
     std::string str(str_arg);
-    std::list<std::pair<T,T> > rv(0,0);
-    char token;
-    std::string data;
-    size_t index;
     if(str.length() < 2) return rv; // throw e
 
-    // check upper bound type
-    token = str[0];
-    switch(token) {
-    case '[':
-    case '(':
-    default:
-      ;                         // currently doing nothing
-    }
-    str.erase(0, 1);
+    // for future use, current assume both bound types are closed
+    char upper_bound_type = str[0];
+    char lower_bound_type = str[str.length()-1];
 
-    // check the lower bound type
-    token = str[str.length()-1];
-    switch(token) {
-    case '[':
-    case '(':
-    default:
-      ;                         // currently doing nothing
-    }
-    str.erase(str.length()-1, 1);
-    
+    // get the two range boundary values
     std::vector<string> fields;
-    boost::split(fields, str, boost::is_any_of(":"), boost::token_compress_on);
+    boost::split(fields, str, boost::is_any_of("[:]"), boost::token_compress_on);
     switch(fields.size()) {
-    case 0: return rv;
-    case 1: return 
+    case 0: {
+      return RangeElement<T>();
     }
-    
-    
-    // get the upper bound
-    index = str.find(':');
-    
-    // throw e when index == npos
-    data = str.sub
-
+    case 1: {
+      return RangeElement<T>(boost::lexi_cast<T>(fields[0]));
+    }
+    case 2: {
+      return RangeElement<T>(boost::lexi_cast<T>(fields[0]),
+                             boost::lexi_cast<T>(fields[1])
+                             );
+    }
+    default:                   // error, should throw e
+      return RangeElement<T>();
+    }
   }
 
   // parse a simple range vector
   template<class T>
-  std::list<std::pair<T,T> > range_vector_paser(const std::string& str) {
+  std::list<RangeElement<T> > paser_range_list(const std::string& str) {
+    // parse something like [5:3][2:1]
     
+    std::list<RangeElement<T> > rv;
+    
+    // pattern to find a range
+    boost::xpressive::sregex token = ('[' | '(') >> [0-9.:]* >> (')' | ']'); 
 
+    boost::xpressive::wsregex_iterator cur(str.begin(), str.end(), token);
+    boost::xpressive::wsregex_iterator end;
+
+    for(; cur != end; ++cur) {
+      const boost::xpressive::smatch& what = *cur;
+      rv.push_back(parse_range(what[0]));
+    }
+    
+    return rv;
   }
-
 
 }
 
